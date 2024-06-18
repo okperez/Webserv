eb5f38af6364d1426541e9543a61e4430fa2dff2
ecnaraga <garance.lambey@gmail.com>
2024-06-18 14:12:44 +0200

    locations ok => a implementer pages d erreur

--- src/request/request.cpp
+++ src/request/request.cpp
@@ -6,7 +6,7 @@
 /*   By: garance <garance@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 14:00:36 by operez            #+#    #+#             */
-/*   Updated: 2024/06/18 11:11:13 by garance          ###   ########.fr       */
+/*   Updated: 2024/06/18 14:11:37 by garance          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,78 +86,114 @@ std::string look_for_location(std::string &target, t_conf & conf) {
 	}
 }
 
+/* Supprime la location et la remplace par la root */
+void	add_path(std::string &target, t_conf & conf, std::string &index) {
+	
+	if (conf.location[index].find("root") == conf.location[index].end())  {
+		std::cout << "Implement with global root" << std::endl;
+		return ;
+	}
+	else if(index.back() == '/' && conf.location[index]["root"].back() != '/')
+		conf.location[index]["root"] += "/";
+	target.replace(target.find(index), index.length(), conf.location[index]["root"]);
+	std::cout << "target : " << target << std::endl;
+}
+
 //  parse request from client and send back response 
 int  handle_request(int socket_fd, t_request & request, t_conf & conf)
 {
-  const char *response_true = "HTTP/1.1 200 OK\r\n\r\n";
-  const char *response_false = "HTTP/1.1 404 Not Found\r\n\r\n";
+	const char *response_true = "HTTP/1.1 200 OK\r\n\r\n";
+	const char *response_false = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-  /* A EFFACER UNE FOIS PARSING DONE */
+	/* A EFFACER UNE FOIS PARSING DONE */
 	std::cout << "TEST0" << std::endl;
-  std::map<std::string, std::string> tmp;
-  tmp["root"] = "pages";
-  conf.location["=/lol"] = tmp; 
-  conf.location["=/lol/"] = tmp; 
-  conf.location["=/lol/index.html"] = tmp; 
-  conf.location["/"] = tmp; 
+	std::map<std::string, std::string> tmp1;
+	tmp1["root"] = "pages";
+	conf.location["/lol"] = tmp1; 
+	conf.location["=/lol/"] = tmp1; 
+	// conf.location["=/lol/index.html"] = tmp1; => a voir comment on traite la root
+	conf.location["/"] = tmp1; 
 	std::cout << "TEST1" << std::endl;
 
-  /* ********************************* */
+	/* ********************************* */
 
-  std::string index = look_for_location(request.target, conf); // si pas trouve index = ""
-  if (request.target == "/")
-  {
-    write(socket_fd, response_true, strlen(response_true));
-    return (1);
-  }
-  else if (request.target == "/index.html")
-  {
-    std::ifstream     file;
-    std::stringstream stream;
-    std::string       tmp;
+	std::string index = look_for_location(request.target, conf); // si pas trouve index = ""
+	if (index.empty())
+	{
+		/* POUR L INSTANT => A TRAITER PLUS TARD VOIR SI ROOT GLOBAL SINON 404 NOT FOUND PEUT ETRE? */
+		write(socket_fd, response_true, strlen(response_true));
+		return (1);
+	}
+	add_path(request.target, conf, index);
+	
+	std::ifstream     file;
+	std::stringstream stream;
+	std::string       tmp;
 
-    file.open("index.html");
-    if (file.is_open())
-      std::getline(file, request.body, '\0');
-    request.lenght = request.body.size();
-    std::string response_content = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: ";
-    stream << request.lenght;
-    stream >> tmp;
-    response_content += tmp + "\r\n\r\n" + request.body;
-    write(socket_fd, response_content.c_str(), response_content.size());
-    return (1);
-  }
-  else if (request.target.substr(0, request.target.rfind('/')) == "/echo")
-  {
-    std::string print = request.target.substr(request.target.rfind('/') + 1, request.target.find('\0') -1);
-    std::string response_content = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
+	file.open(request.target);
+	if (file.is_open()) //GERER ERREUR si pas bon
+		std::getline(file, request.body, '\0');
+	request.lenght = request.body.size();
+	std::string response_content = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: ";
+	stream << request.lenght;
+	stream >> tmp;
+	response_content += tmp + "\r\n\r\n" + request.body;
+	write(socket_fd, response_content.c_str(), response_content.size());
+	return (1);
+		
+	// if (request.target == "/")
+	// {
+	// 	write(socket_fd, response_true, strlen(response_true));
+	// 	return (1);
+	// }
+	// else if (request.target == "/index.html")
+	// {
+		// std::ifstream     file;
+		// std::stringstream stream;
+		// std::string       tmp;
 
-    std::stringstream stream;
-    std::string       tmp;
-    request.lenght = print.size();
-    stream << print.size();
-    stream >> tmp;
-    response_content += tmp + "\r\n\r\n" + print;
-    request.body = response_content;
-    write(socket_fd, request.body.c_str(), request.body.size());
-    return (1);
-  }
-  else if ((request.target == "/user-agent"))
-  {
-    std::stringstream stream;
-    std::string       tmp;
+		// file.open("index.html");
+		// if (file.is_open())
+		// 	std::getline(file, request.body, '\0');
+		// request.lenght = request.body.size();
+		// std::string response_content = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: ";
+		// stream << request.lenght;
+		// stream >> tmp;
+		// response_content += tmp + "\r\n\r\n" + request.body;
+		// write(socket_fd, response_content.c_str(), response_content.size());
+		// return (1);
+	// }
+	// else if (request.target.substr(0, request.target.rfind('/')) == "/echo")
+	// {
+	// 	std::string print = request.target.substr(request.target.rfind('/') + 1, request.target.find('\0') -1);
+	// 	std::string response_content = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
 
-    request.body = request.agent;
-    request.lenght = request.body.size();
-    std::string response_content = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
-    stream << request.lenght;
-    stream >> tmp;
-    response_content += tmp + "\r\n\r\n" + request.body;
-    write(socket_fd, response_content.c_str(), response_content.size());
-    return (1);
-  }
-  write(socket_fd, response_false, strlen(response_false));
-  return (0);
+	// 	std::stringstream stream;
+	// 	std::string       tmp;
+	// 	request.lenght = print.size();
+	// 	stream << print.size();
+	// 	stream >> tmp;
+	// 	response_content += tmp + "\r\n\r\n" + print;
+	// 	request.body = response_content;
+	// 	write(socket_fd, request.body.c_str(), request.body.size());
+	// 	return (1);
+	// }
+	// else if ((request.target == "/user-agent"))
+	// {
+	// 	std::stringstream stream;
+	// 	std::string       tmp;
+
+	// 	request.body = request.agent;
+	// 	request.lenght = request.body.size();
+	// 	std::string response_content = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
+	// 	stream << request.lenght;
+	// 	stream >> tmp;
+	// 	response_content += tmp + "\r\n\r\n" + request.body;
+	// 	write(socket_fd, response_content.c_str(), response_content.size());
+	// 	return (1);
+	// }
+	write(socket_fd, response_false, strlen(response_false));
+	return (0);
 }
 
 // TEST OK
